9a2482621389825c6e5b6fe6029b5b200ff31b90
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-10-06 12:48:22 +0700

    refactor: move project files back to root folder

---
+++ src/main/java/com/windear/app/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.windear.app.controller;
+
+import com.windear.app.model.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class HomeController {
+   @GetMapping("/books")
+   public List<Book> getBooks() {
+      return Arrays.asList(
+            new Book(
+                  1,
+                  "Lap trinh can ban",
+                  "Phong",
+                  "khong biet",
+                  "69"
+            )
+            
+      );
+   }
+   
+   // Có thể mapping thêm các endpoint khác nữa...
+}
