8aeae9058e9b25e1deef0166fdaeb5a27207dfe5
Quan <quan2082005@gmail.com>
2024-10-14 22:18:28 +0700

    feat: fix news api

--- src/main/java/com/windear/app/controller/NewsController.java
+++ src/main/java/com/windear/app/controller/NewsController.java
@@ -5,6 +5,8 @@ import com.windear.app.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class NewsController {
@@ -20,9 +22,19 @@ public class NewsController {
         return newsService.saveNews(news);
     }
 
+    @GetMapping("/news")
+    public List<News> getNewsList() {
+        return newsService.findAllNews();
+    }
+
     @GetMapping("/news/{newsId}")
     public News getNews(@PathVariable int newsId) {
         return newsService.findNewsById(newsId);
+    }
+
+    @PutMapping("/news")
+    public News updateNews(@RequestBody News news) {
+        return newsService.update(news);
     }
 
     @DeleteMapping("news/{newsId}")
--- src/main/java/com/windear/app/service/NewsService.java
+++ src/main/java/com/windear/app/service/NewsService.java
@@ -2,8 +2,12 @@ package com.windear.app.service;
 
 import com.windear.app.entity.News;
 
+import java.util.List;
+
 public interface NewsService {
     News saveNews(News news);
+    List<News> findAllNews();
     News findNewsById(int newsId);
     void deleteNews(int newsId);
+    News update(News news);
 }
