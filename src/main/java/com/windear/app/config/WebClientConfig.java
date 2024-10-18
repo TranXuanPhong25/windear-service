fa824bbb0060e4dc4eecfb643858608d7bca7f55
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-10-18 22:04:08 +0700

    chore: add TODO

--- src/main/java/com/windear/app/config/WebClientConfig.java
+++ src/main/java/com/windear/app/config/WebClientConfig.java
@@ -16,6 +16,7 @@ public class WebClientConfig {
                 .build();
         return WebClient.builder()
                 .baseUrl("https://api.hardcover.app/v1/graphql")
+                //TODO: move access token to .env
                 .defaultHeader("authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoiNiIsImlkIjoxOTY5NywibG9nZ2VkSW4iOnRydWUsInN1YiI6IjE5Njk3IiwiaWF0IjoxNzI4OTExMzE5LCJleHAiOjE3NjAzNjEyMTksImh0dHBzOi8vaGFzdXJhLmlvL2p3dC9jbGFpbXMiOnsieC1oYXN1cmEtYWxsb3dlZC1yb2xlcyI6WyJ1c2VyIl0sIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1yb2xlIjoidXNlciIsIlgtaGFzdXJhLXVzZXItaWQiOiIxOTY5NyJ9fQ.ZGmkEjsScAuK5PEz12dBT6s64-iabXhlubeG9UGjyqE")
                 .exchangeStrategies(strategies)
                 .build();
