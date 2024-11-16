04de1fe72823eaeb3bc6046ebde9694ebcb56a53
mquang09 <101109835+mquang09@users.noreply.github.com>
2024-11-16 20:21:04 +0700

    fix: change variable type

--- src/main/java/com/windear/app/service/ExternalBookService.java
+++ src/main/java/com/windear/app/service/ExternalBookService.java
@@ -11,6 +11,6 @@ public interface ExternalBookService {
     String getSearchSuggestions(String q);
     String getFeaturedBookLists();
     String getPopularBookLists();
-    String getSimilarBooks(int id);
+    String getSimilarBooks(String id);
     String getEditions(String id);
 }
