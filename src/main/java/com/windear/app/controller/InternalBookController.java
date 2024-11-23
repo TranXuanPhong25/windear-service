2cdcd3f74a6d5dea1538f53f30961a9608b44673
Quan <quan2082005@gmail.com>
2024-11-23 16:52:33 +0700

    feat: add internal book dto

--- src/main/java/com/windear/app/controller/InternalBookController.java
+++ src/main/java/com/windear/app/controller/InternalBookController.java
@@ -1,26 +1,39 @@
 package com.windear.app.controller;
 
+import com.windear.app.dto.AddInternalBookRequestDTO;
+import com.windear.app.dto.InternalBookDTO;
 import com.windear.app.entity.Book;
+import com.windear.app.entity.BookGenre;
 import com.windear.app.entity.InternalBook;
+import com.windear.app.primarykey.BookGenreId;
+import com.windear.app.service.BookGenreService;
 import com.windear.app.service.InternalBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/db")
 public class InternalBookController {
     private final InternalBookService bookService;
+    private final BookGenreService bookGenreService;
 
     @Autowired
-    public InternalBookController(InternalBookService bookService) {
+    public InternalBookController(InternalBookService bookService, BookGenreService bookGenreService) {
         this.bookService = bookService;
+        this.bookGenreService = bookGenreService;
     }
 
     @GetMapping("/books")
-    public List<InternalBook> findAll() {
-        return bookService.findAll();
+    public List<InternalBookDTO> findAll() {
+        List<InternalBook> books = bookService.findAll();
+        List<InternalBookDTO> booksDto = new ArrayList<>();
+        for(InternalBook it : books) {
+            booksDto.add(convertToDTO(it));
+        }
+        return booksDto;
     }
 
     @GetMapping("/books/{id}")
@@ -39,12 +52,31 @@ public class InternalBookController {
     }
 
     @PostMapping("/books")
-    public InternalBook addBook(@RequestBody InternalBook book) {
-        return bookService.add(book);
+    public InternalBook addBook(@RequestBody AddInternalBookRequestDTO request) {
+        /*String[] genreIds = request.getGenres().split(",");
+        for (String genreId : genreIds) {
+            BookGenre bookGenre = new BookGenre();
+            BookGenreId bookGenreId = new BookGenreId(request.getInternalBook().getBookId(), Integer.parseInt(genreId.trim()));
+            bookGenre.setBookGenreId(bookGenreId);
+            bookGenreService.add(bookGenre);
+        }*/
+        return bookService.add(request.getInternalBook());
     }
 
     @DeleteMapping("/books/{id}")
     public void deleteBook(@PathVariable Integer id) {
         bookService.delete(id);
+    }
+
+
+    private InternalBookDTO convertToDTO(InternalBook book) {
+        InternalBookDTO dto = new InternalBookDTO();
+        dto.setId(book.getBookId());
+        dto.setTitle(book.getTitle());
+        dto.setAuthor(book.getAuthor());
+        dto.setPublisher(book.getPublisher());
+        dto.setAddDate(book.getAddDate());
+        dto.setReleaseDate(book.getReleaseDate());
+        return dto;
     }
 }
---
+++ src/main/java/com/windear/app/dto/AddInternalBookRequestDTO.java
@@ -0,0 +1,32 @@
+package com.windear.app.dto;
+
+import com.windear.app.entity.InternalBook;
+import jakarta.persistence.Column;
+
+import java.time.LocalDate;
+
+public class AddInternalBookRequestDTO {
+    private InternalBook internalBook;
+    private String genres;
+
+    public AddInternalBookRequestDTO(InternalBook internalBook, String genres) {
+        this.internalBook = internalBook;
+        this.genres = genres;
+    }
+
+    public InternalBook getInternalBook() {
+        return internalBook;
+    }
+
+    public void setInternalBook(InternalBook internalBook) {
+        this.internalBook = internalBook;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+}
