c378ea73214f1fb0c72227ff83775ad751f7b030
mquang09 <101109835+mquang09@users.noreply.github.com>
2024-11-28 17:17:43 +0700

    docs: add javadoc for AnalyticStatService, Auth0LogService, Auth0ManagementService

--- src/main/java/com/windear/app/service/AnalyticStatService.java
+++ src/main/java/com/windear/app/service/AnalyticStatService.java
@@ -5,7 +5,21 @@ import com.windear.app.model.AnalyticStat;
 import java.util.List;
 
 public interface AnalyticStatService {
+    /**
+     * Get add stats of internal book in 30 days
+     * @return list of internal book stat
+     */
     List<AnalyticStat> getStatsOfInternalBookIn30Days();
+
+    /**
+     * Get statistics of borrow requests in the last 30 days.
+     * @return list of borrow request statistics
+     */
     List<AnalyticStat> getStatsOfBorrowRequestIn30Days();
+
+    /**
+     * Get statistics of return requests in the last 30 days.
+     * @return list of return request statistics
+     */
     List<AnalyticStat> getStatsOfReturnRequestIn30Days();
 }
--- src/main/java/com/windear/app/service/Auth0ManagementService.java
+++ src/main/java/com/windear/app/service/Auth0ManagementService.java
@@ -8,11 +8,50 @@ import java.util.List;
 
 @Service
 public interface Auth0ManagementService {
+    /**
+     * Retrieves a list of users.
+     * @return a list of users
+     */
     String getUsers();
+
+    /**
+     * Deletes a user by their ID.
+     * @param id the ID of the user to delete
+     * @return a ResponseEntity with the result of the deletion
+     */
     ResponseEntity<String> deleteUser(String id);
+
+    /**
+     * Resets the password for a user by their ID.
+     * @param id the ID of the user whose password is to be reset
+     * @return a ResponseEntity with the result of the password reset
+     */
     ResponseEntity<String> resetPassword(String id);
+
+    /**
+     * Updates the profile of a user by their ID.
+     * @param userId the ID of the user whose profile is to be updated
+     * @param data the new profile data
+     * @return a ResponseEntity with the result of the profile update
+     */
     ResponseEntity<String> updateProfile(String userId, String data);
+
+    /**
+     * Resends the verification email to a user by their ID.
+     * @param userId the ID of the user to resend the verification email to
+     * @return a ResponseEntity with the result of the email resend
+     */
     ResponseEntity<String> resendVerificationEmail(String userId);
+
+    /**
+     * Retrieves a list of active users.
+     * @return a ResponseEntity containing the list of active users
+     */
     ResponseEntity<String> getActiveUsers();
+
+    /**
+     * Retrieves a list of logs.
+     * @return a ResponseEntity containing the logs
+     */
     ResponseEntity<?> getLogs();
 }
